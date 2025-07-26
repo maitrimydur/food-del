6397e8ca19680524740763476d704c126d7d75a2
Maitri Mydur <mmydur@gmail.com>
2025-07-25 17:48:27 -0700

    Fix Sidebar NavLink closing tags and add routes

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
-        <div className="sidebar-option">
+        <NavLink className="sidebar-option">
           <img src={assets.add_icon} alt="" />
           <p>Add Items</p>
-        </div>
+        </NavLink>
         <div className="sidebar-option">
           <img src={assets.order_icon} alt="" />
           <p>List Items</p>
