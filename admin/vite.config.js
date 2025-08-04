a625f2b99408a00552e79275819cc64b6c6a8871
Maitri Mydur <mmydur@gmail.com>
2025-08-04 12:39:16 -0700

    Need to redo backend

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy API calls to your Express backend
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+      // Proxy image requests too
+      '/images': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
 })
