014377bca1f1185fe652f33fc398bd87b6aee7aa
Maitri Mydur <mmydur@gmail.com>
2025-08-04 12:46:24 -0700

    Made some changes in admin folder specifically vite.config.js and List.jsx to try to make images appear from backend folder

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,51 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import './List.css'
-import axios from "axios"
-import {toast} from "react-toastify"
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const List = () => {
+  const [list, setList] = useState([])
 
-  const url = "http://localhost:4000"
-  const [_list,_setList] = useState([]);
-
-  const _fetchList = async () => {
-    const _response = await axios.get(`${url}/api/food/list`)
-    console.log(_response.data);
-    
-    if (_response.data.success) {
-      _setList(_response.data.data)
-    }
-    else 
-    {
-      toast.error("Error")
+  const fetchList = async () => {
+    try {
+      const { data } = await axios.get('/api/food/list')
+      if (data.success) {
+        setList(data.data)
+      } else {
+        toast.error('Error fetching list')
+      }
+    } catch (err) {
+      console.error(err)
+      toast.error('Network error')
     }
   }
-  useEffect(()=>{
-    _fetchList();
+
+  useEffect(() => {
+    fetchList()
   }, [])
 
   return (
-    <div className = 'list add flex-col'>
+    <div className='list add flex-col'>
       <p>All Foods List</p>
-      <div className="list-table">
-        <div className="list-table-format title" >
-            <b>Image</b>
-            <b>Name</b>
-            <b>Cateogry</b>
-            <b>Price</b>
-            <b>Action</b>
+      <div className='list-table'>
+        <div className='list-table-format title'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
         </div>
-        {_list.map((item,index)=>{
-            return (
-              <div key={index} className='list-table-format'>
-                  <img src={`${url}/images/` +item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>{item.category}</p>
-                  <p>${item.price}</p>
-                  <p>X</p>
-              </div>
-            )
-        })}
+        {list.map((item, i) => (
+          <div key={i} className='list-table-format'>
+            <img src={`/images/${item.image}`} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>${item.price}</p>
+            <p>X</p>
+          </div>
+        ))}
       </div>
     </div>
   )
