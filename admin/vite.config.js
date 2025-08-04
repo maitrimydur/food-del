92b5613efec6e13530130a775d0d21d4747fc122
Maitri Mydur <mmydur@gmail.com>
2025-08-04 12:46:24 -0700

    Made some changes in admin folder specifically vite.config.js and List.jsx to try to make images appear from backend folder

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API calls to your Express backend
       '/api': {
         target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
       },
-      // Proxy image requests too
       '/images': {
         target: 'http://localhost:4000',
         changeOrigin: true,
