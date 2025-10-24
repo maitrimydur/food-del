fb171e675188025827d80dcb244890b58bd293c9
Maitri Mydur <mmydur@gmail.com>
2025-10-24 10:00:16 -0700

    Current errors

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+const authMiddleware = async (req,resizeBy,next) => {
+    const {token} = req.headers;
+    if (!token) {
+        return resizeBy.json({success:false,message:"Not Authorized Login Again"})
+    }
+    try {
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
+        req.body.userID = token_decode.id;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export default authMiddleware;
